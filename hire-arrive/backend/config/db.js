16673a136251ddd291f9b6801847f05d7475c3fd
Yathish Acharya <yathishkumar2013@gmail.com>
2024-09-24 22:05:56 +0530

    heroku

--- hire-arrive/backend/config/db.js
+++ hire-arrive/backend/config/db.js
@@ -1,9 +1,13 @@
 // config/db.js
 import mongoose from 'mongoose';
+dotenv.config();
+
+const dbURI = process.env.MONGODB_URI;
+
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://iamyathz:Hirearrive123@cluster0.lorwk.mongodb.net/s'); // Removed deprecated options
+    await mongoose.connect(dbURI); 
     console.log('MongoDB connected');
   } catch (err) {
     console.error('Error connecting to MongoDB', err.message);
