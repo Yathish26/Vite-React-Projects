0e13534b78ec57f00384748b7325df22f43a72e8
Yathish Acharya <yathishkumar2013@gmail.com>
2024-09-24 15:18:25 +0530

    mongocluster

--- hire-arrive/backend/config/db.js
+++ hire-arrive/backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/users'); // Removed deprecated options
+    await mongoose.connect('mongodb+srv://iamyathz:Hirearrive123@cluster0.lorwk.mongodb.net/s'); // Removed deprecated options
     console.log('MongoDB connected');
   } catch (err) {
     console.error('Error connecting to MongoDB', err.message);
