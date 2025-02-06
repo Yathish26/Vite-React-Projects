60e85964ce5e82a6144bb001e8da66699beeabeb
Yathish Acharya <yathishkumar2013@gmail.com>
2025-02-06 16:07:53 +0530

    Removed Trash

--- hire-arrive/src/Data/sheet.js
+++
@@ -1,4 +0,0 @@
-const DB = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQh_84xbQSq6BK_d97FBmEfQxDbdudWaM_Y-MRbgq6a48kkBVc2e5sR4uIAIIlfGNJfFeYf8czRrY-r/pub?output=csv';
-// const DB = '/data/Format Data.csv';
-
-export default DB;
--- hire-arrive/src/Editable Tool/contact.js
+++
@@ -1,8 +0,0 @@
-const Contact = {
-    email : "hirearrive@proton.me",
-    Linkedin : "https://www.linkedin.com/company/hire-arrive/",
-    Instagram : "https://www.instagram.com/hire_arrive/",
-    Website : "https://www.hirearrive.in/",
-}
-
-export default Contact
